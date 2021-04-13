276cdc36dff81b188e2279e6febb8905b7a46bf4
赵人杰 <zhaorenjie@pandadastudio.com>
2021-04-13 10:39:52 +0800

    modal导入方式

--- packages/theme/src/services/modal/modal.helper.ts
+++ packages/theme/src/services/modal/modal.helper.ts
@@ -1,4 +1,4 @@
-import { Modal } from "ant-design-vue";
+import Modal from "ant-design-vue/lib/modal";
 import { ModalFuncProps } from "ant-design-vue/lib/modal";
 import { h, isVNode, render, VNode } from "vue";
 
