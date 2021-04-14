99d6ce97bbaa79a4d80a4e95d269606780eb2500
赵人杰 <zhaorenjie@pandadastudio.com>
2021-04-14 11:16:51 +0800

    暂时不需要files

---
+++ packages/theme/src/src/services/settings/type.d.ts
@@ -0,0 +1,19 @@
+export interface IApp {
+    name: string;
+    description: string;
+    [key: string]: any;
+}
+export interface IUser {
+    id: number;
+    name: string;
+    phone: string;
+    account: string;
+    [key: string]: any;
+}
+export interface ILayout {
+    collapse: boolean;
+}
+export interface IProject {
+    id: number;
+    name: string;
+}
---
+++ packages/theme/src/src/services/validator/index.d.ts
@@ -0,0 +1,2 @@
+export declare function initSchema(): void;
+export declare const CUSTOM_TRIGGER = "custom";
