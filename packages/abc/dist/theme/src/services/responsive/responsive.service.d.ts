7799e4816cf195d6b3d6ada711e55543f9e66b72
赵人杰 <zhaorenjie@pandadastudio.com>
2021-05-06 18:31:01 +0800

    abc打包

--- packages/abc/dist/theme/src/services/responsive/responsive.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { ResponsiveConfig } from "./type";
-export declare class ResponsiveService {
-    private config;
-    constructor(config?: ResponsiveConfig);
-    genClass(count: number): string[];
-}
-export declare const responseService: ResponsiveService;
