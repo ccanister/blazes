29cad2604435ca9a4f6427a6633ed169cc6d22ac
赵人杰 <zhaorenjie@pandadastudio.com>
2021-07-01 15:30:16 +0800

    feat: abc库默认导出
    affects: @blazes/abc

--- packages/abc/index.ts
+++ packages/abc/index.ts
@@ -1,15 +1,27 @@
-import Ellipsis from "./src/ellipsis";
 import st from "./src/st";
 import PageHeader from "./src/page-header";
 import Operates from "./src/operates";
 import SearchInput from "./src/search-input";
 import sv from "./src/sv";
-import sf, { getData, transMapToEnum, formSymbol, formRefSymbol } from "./src/sf";
+import sf, {
+  getData,
+  transMapToEnum,
+  formSymbol,
+  formRefSymbol,
+} from "./src/sf";
 import TableWrapper from "./src/table-wrapper";
 import Tags from "./src/tags";
+import { App } from "vue";
+
+const components = [st, PageHeader, Operates, sv, TableWrapper, sf, Tags];
+
+const install = (app: App, options: Record<string, unknown>): void => {
+  components.forEach((component) => {
+    app.component(component.name, component);
+  });
+};
 
 export {
-  Ellipsis,
   st,
   PageHeader,
   Operates,
@@ -23,3 +35,5 @@ export {
   formSymbol,
   formRefSymbol,
 };
+
+export default { install };
