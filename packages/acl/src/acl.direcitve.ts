6f05397eb6676d8cac7cf41638538b4278b605c0
赵人杰 <zhaorenjie@pandadastudio.com>
2021-07-13 17:12:41 +0800

    feat: 指令监听acl角色变化
    affects: @blazes/acl

--- packages/acl/src/acl.direcitve.ts
+++ packages/acl/src/acl.direcitve.ts
@@ -1,5 +1,6 @@
 import { aclService } from "./acl.service";
 import { IACLCanType } from "./type";
+import { watch } from "vue";
 
 function updateEl(el: HTMLElement, binding: { value: IACLCanType }) {
   el.style.display = ((aclService.can(binding.value)
@@ -10,6 +11,9 @@ function updateEl(el: HTMLElement, binding: { value: IACLCanType }) {
 export class AclDirecitve {
   beforeMount(el: HTMLElement, binding: { value: IACLCanType }) {
     updateEl(el, binding);
+    watch(aclService.aclChange, () => {
+      updateEl(el, binding);
+    });
   }
 
   updated(el: HTMLElement, binding: { value: IACLCanType }) {
