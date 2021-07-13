cb710708bc9d04cab7f6b2717897a13832274578
赵人杰 <zhaorenjie@pandadastudio.com>
2021-07-13 17:23:35 +0800

    feat: 路由守卫改成局部
    affects: @blazes/acl

--- packages/acl/src/acl-grard.ts
+++ packages/acl/src/acl-grard.ts
@@ -1,18 +1,20 @@
-import { Router } from "vue-router";
+import { NavigationGuardNext, RouteLocationNormalized } from "vue-router";
 import { aclService } from "./acl.service";
 import { IACLCanType } from "./type";
 
-export function aclGuard(router: Router) {
-  router.beforeEach((to, from, next) => {
-    const guard = to.meta.guard as string[] | IACLCanType;
-    if (!guard) {
-      next();
-      return;
-    }
-    if (aclService.can(Array.isArray(guard) ? { roles: guard } : guard)) {
-      next();
-      return;
-    }
-    next(aclService.guard_url);
-  });
+export function aclGuard(
+  to: RouteLocationNormalized,
+  from: RouteLocationNormalized,
+  next: NavigationGuardNext
+) {
+  const guard = to.meta.guard as string[] | IACLCanType;
+  if (!guard) {
+    next();
+    return;
+  }
+  if (aclService.can(Array.isArray(guard) ? { roles: guard } : guard)) {
+    next();
+    return;
+  }
+  next(aclService.guard_url);
 }
