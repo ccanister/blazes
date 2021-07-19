572f80a88262f3d32a99f3f8696ed32bb33b0317
赵人杰 <zhaorenjie@pandadastudio.com>
2021-07-19 16:51:56 +0800

    feat: 类型提示
    affects: @blazes/abc

--- packages/abc/build/gen-type.js
+++ packages/abc/build/gen-type.js
@@ -1,21 +1,8 @@
-const fs = require("fs");
 const { copyDir, resolvePath } = require("./util");
 const { clean } = require("./clean");
 
 const dirs = ["abc", "theme", "src"];
 
-copyDir(resolvePath(`../lib/abc/src`), resolvePath("../lib/src"));
-const files = fs.readdirSync(resolvePath("../lib"));
-
-files.forEach((name) => {
-  if (dirs.includes(name)) {
-    return;
-  }
-  const tarDir = resolvePath(`../lib/${name}`);
-  if (!fs.statSync(tarDir).isDirectory()) {
-    return;
-  }
-  copyDir(resolvePath(`../lib/src/${name}`), tarDir);
-});
-
+copyDir(resolvePath(`../lib/abc`), resolvePath("../lib"));
+copyDir(resolvePath(`../lib/abc/src`), resolvePath("../lib"));
 clean(dirs);
