539389d34eafb3c990eefe7c2aed53b26af65b1e
赵人杰 <zhaorenjie@pandadastudio.com>
2021-08-09 17:44:20 +0800

    style: 改用rollup打包
    affects: @blazes/theme

---
+++ packages/theme/rollup.config.js
@@ -0,0 +1,73 @@
+import typescript from "@rollup/plugin-typescript";
+import clear from "rollup-plugin-clear";
+import { terser } from "rollup-plugin-terser";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "rollup-plugin-babel";
+import copy from "rollup-plugin-copy";
+import postcss from "rollup-plugin-postcss";
+import NpmImport from "less-plugin-npm-import";
+import cssnano from "cssnano";
+
+export default [
+  {
+    input: "./index.ts",
+    output: {
+      file: "dist/index.js",
+      name: "blazeTheme",
+      format: "umd",
+      sourcemap: true,
+      globals: {
+        vue: "vue",
+        axios: "axios",
+        "async-validator": "async-validator",
+        "vue-router": "vue-router",
+      },
+    },
+    plugins: [
+      babel({
+        exclude: "node_modules/**",
+      }),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      clear({
+        targets: ["dist"],
+      }),
+      copy({
+        targets: [{ src: "system/*", dest: "dist" }],
+      }),
+      terser(),
+    ],
+    external: [
+      "vue",
+      "axios",
+      "async-validator",
+      "vue-router",
+      /^@blazes\/utils/,
+      /^ant-design-vue\/lib/,
+      /^@ant-design\/icons-vue/,
+    ],
+  },
+  {
+    input: "./system/index.less",
+    output: {
+      file: "dist/deprecated.style.js",
+    },
+    plugins: [
+      postcss({
+        use: [
+          [
+            "less",
+            {
+              plugins: [new NpmImport({ prefix: "~" })],
+              javascriptEnabled: true,
+            },
+          ],
+        ],
+        extract: "style.css",
+        plugins: [cssnano()],
+      }),
+    ],
+  },
+];
