3accd30c26692309e1d5c018eb36dff2018a583e
赵人杰 <zhaorenjie@pandadastudio.com>
2021-08-09 16:15:56 +0800

    style: extends模块
    affects: @blazes/utils

--- packages/utils/rollup.config.js
+++ packages/utils/rollup.config.js
@@ -1,6 +1,8 @@
 import typescript from "@rollup/plugin-typescript";
 import clear from "rollup-plugin-clear";
 import { terser } from "rollup-plugin-terser";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default {
   input: "./index.ts",
@@ -9,12 +11,19 @@ export default {
     name: "blazeUtils",
     format: "umd",
     sourcemap: true,
+    globals: {
+      vue: "vue",
+      extend: "extend",
+    },
   },
   plugins: [
+    resolve(),
+    commonjs(),
     typescript({ tsconfig: "./tsconfig.json" }),
     clear({
       targets: ["dist"],
     }),
     terser(),
   ],
+  external: ["vue", "extend"],
 };
