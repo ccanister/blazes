ce4683ee714a226614e97d7f4cee4764a04564d3
赵人杰 <zhaorenjie@pandadastudio.com>
2021-08-23 10:50:01 +0800

    fix: 不拷贝sf-default文件
    affects: @blazes/abc

--- packages/abc/build/clean.js
+++ packages/abc/build/clean.js
@@ -1,9 +1,8 @@
 const rimraf = require("rimraf");
-const { resolvePath } = require("./util");
 
 function clean(dirs) {
   dirs.forEach((dir) => {
-    rimraf.sync(resolvePath(`../lib/${dir}`));
+    rimraf.sync(dir);
   });
 }
 
