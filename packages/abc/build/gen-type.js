f122f27ca5241bd74b9f90af5f2f1749d707bec6
赵人杰 <zhaorenjie@pandadastudio.com>
2021-08-23 10:26:05 +0800

    feat: 打包脚本
    affects: @blazes/abc

--- packages/abc/build/gen-type.js
+++ packages/abc/build/gen-type.js
@@ -1,26 +1,7 @@
 const { copyDir, resolvePath } = require("./util");
 const { clean } = require("./clean");
-const fs = require("fs");
 
 const dirs = ["abc", "theme", "src"];
 
-// 有时候打出来的类型会在abc里面，有时候又会在src里面
-try {
-  copyDir(resolvePath(`../lib/abc`), resolvePath("../lib"));
-  copyDir(resolvePath(`../lib/abc/src`), resolvePath("../lib"), (tarDir) => {
-    return tarDir === resolvePath(`../lib/sf/src/widgets`);
-  });
-} catch (error) {
-  const files = fs.readdirSync(resolvePath("../lib"));
-  files.forEach((name) => {
-    if (dirs.includes(name)) {
-      return;
-    }
-    const tarDir = resolvePath(`../lib/${name}`);
-    if (!fs.statSync(tarDir).isDirectory()) {
-      return;
-    }
-    copyDir(resolvePath(`../lib/src/${name}`), tarDir);
-  });
-}
+copyDir(resolvePath(`../lib/src`), resolvePath("../lib"));
 clean(dirs);
