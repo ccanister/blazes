7a9b0c00f35d5ba7f91fbde63ec7f6e090414271
zhaorenjie <zhaorenjie@pandadastudio.com>
2022-11-16 16:39:32 +0800

    fix: 类型文件未打包
    affects: @blazes/abc

--- packages/abc/build/gen-type.js
+++ packages/abc/build/gen-type.js
@@ -1,9 +1,9 @@
 const { copyDir, resolvePath } = require("./util");
 const { clean } = require("./clean");
 
-const dirs = ["abc"];
+const dirs = ["abc", "src"];
 
-copyDir(resolvePath(`../lib/abc/src`), resolvePath("../lib"));
+copyDir(resolvePath(`../lib/abc`), resolvePath("../lib"));
 clean([
   ...dirs.map((dir) => `${resolvePath(`../lib/${dir}`)}`),
   resolvePath("../lib/sf/src/widgets/sf-default.vue"),
